f1c2a34a17360dd4787d29452ede1ac244f3ee00
Özgün Özdemir <ozgunozdemir92@gmail.com>
2020-05-31 14:45:19 +0300

    added react-native-vector-icon

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Text, View, Button } from 'react-native'
 import { HeaderComponent } from '../components/Layout/Header.js'
 
+
 function Dashboard({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <HeaderComponent backgroundColor="#1f78d1" headerTitle="Anasayfa" headerTitleColor="#ffffff"/>
+            <HeaderComponent backgroundColor="#1f78d1" headerTitle="Anasayfa" headerTitleColor="#ffffff" iconLeft="true" iconLeft="false" iconLeftColor="#ffffff" iconLeftName="navicon"/>
             <Text>Dashboard Screen</Text>
             <Button onPress={() => navigation.goBack()} title="Go Back"></Button>
         </View>
